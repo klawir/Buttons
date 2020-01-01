66c68670246d9b85eedede7efebf8cb86085a09f
klawir <klawir1@o2.pl>
2020-01-01 18:58:02 +0100

    GUI revision 3
    To program has been added a better assets

---
+++ Assets/program/Data/Objects/UI/Core/Object.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace UI
+{
+    public class Object : AnyObject
+    {
+        public float time;
+        
+        public override void Remove()
+        {
+            Destroy(gameObject, time);
+        }
+    }
+}
